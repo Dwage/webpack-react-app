0e06a2bb7fb5fa65856a2bd70728ac3c7fae9a46
Dwage <SuperMartinSuper@yandex.ru>
2025-02-03 18:14:42 +0400

    webpack configuration

---
+++ config/build/buildDevServer.ts
@@ -0,0 +1,13 @@
+import { BuildOptions } from './types/config'
+
+export function buildDevServer(options: BuildOptions) {
+  const { paths, port } = options
+  return {
+    // static: {
+    //   directory: paths.build,
+    // },
+    //compress: true,
+    port: port,
+    open: true,
+  }
+}
---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import ArithmeticSum from './components/ArithmeticSum'
+import Counter from './components/Counter'
+import './index.scss'
+const App = () => {
+  return (
+    <div className="app">
+      asfjkbasfjkjk
+      <Counter />
+      <ArithmeticSum />
+    </div>
+  )
+}
+
+export default App
