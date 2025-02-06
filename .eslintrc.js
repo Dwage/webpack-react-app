d7c42d73d59f21130ad8fb6d1136e03b1ca9e6bf
Dwage <SuperMartinSuper@yandex.ru>
2025-02-06 18:24:11 +0400

    increase max length for strings of code

--- .eslintrc.js
+++ .eslintrc.js
@@ -32,7 +32,7 @@ module.exports = {
     'no-underscore-dangle': 'off',
     semi: ['error', 'never'],
     'i18next/no-literal-string': ['warn', { markupOnly: true }],
-    'max-len': ['error', { ignoreComments: true }],
+    'max-len': ['error', { code: 100, ignoreComments: true }],
 
   },
   globals: {
