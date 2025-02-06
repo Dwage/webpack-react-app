4cc9096c617a009e6c2dcab5cf7b7b2c57e590aa
Dwage <SuperMartinSuper@yandex.ru>
2025-02-06 18:20:00 +0400

    fix: change parameters for classname test

--- src/shared/lib/classNames.test.ts
+++ src/shared/lib/classNames.test.ts
@@ -31,7 +31,7 @@ describe('classNames', () => {
   })
 
   test('Filters out falsy values in mods', () => {
-    expect(classNames('base-class', { active: false, visible: 0, theme: '' }))
+    expect(classNames('base-class', { active: false, visible: '1', theme: '' }))
       .toBe('base-class')
   })
 })
